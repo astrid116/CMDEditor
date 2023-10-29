c90fc1bfef88e61c9eb67acb7f3c6c60d0b20793
astrid116 <jaffadani@gmail.com>
2023-10-29 22:56:29 +0100

    Uploaded Source Code

---
+++ Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CMDEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            string filename;
+            while (true)
+            {
+                while (true)
+                {
+                    Console.Title = "CMDEditor | Made By Astrid";
+                    Console.Write("Enter file path (hit enter to use current directory): ");
+                    path = Console.ReadLine();
+                    Console.Write("Enter the file name: ");
+                    filename = Console.ReadLine();
+                    if (path == "")
+                    {
+                        if (!File.Exists(Directory.GetCurrentDirectory() + "\\" + filename))
+                        {
+                            Console.WriteLine("The specified file doesn't exists. ");
+                        }
+                        else
+                        {
+                            path = Directory.GetCurrentDirectory() + "\\" + filename;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (!File.Exists(@path + "\\" + filename))
+                        {
+                            Console.WriteLine("The specified file doesn't exists. ");
+                        }
+                        else
+                        {
+                            path = @path + "\\" + filename;
+                            break;
+                        }
+                    }
+                }
+
+
+                Console.Clear();
+                Console.Title = "CMDEditor | Editing: " + path;
+                string lineInput;
+                string line;
+                int lineIndex;
+                while (true)
+                {
+                    Console.Write(">");
+                    lineInput = Console.ReadLine();
+                    string[] arr = lineInput.Split();
+                    if (lineInput == ":q")
+                    {
+                        Console.Clear();
+                        break;
+                    }
+
+                    if (lineInput == "")
+                    {
+                        int i = 1;
+                        var sr = new StreamReader(path);
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line != "")
+                            {
+                                if (i < 10)
+                                {
+                                    Console.WriteLine(i + "    ~  " + line);
+                                }
+                                else if (i < 100)
+                                {
+                                    Console.WriteLine(i + "   ~  " + line);
+                                }
+                                else if (i < 1000)
+                                {
+                                    Console.WriteLine(i + "  ~  " + line);
+                                }
+                                else if (i < 10000)
+                                {
+                                    Console.WriteLine(i + " ~  " + line);
+                                }
+                            }
+                            i++;
+                        }
+                        sr.Close();
+                    }
+                    else if (int.TryParse(arr[0], out lineIndex))
+                    {
+                        string tempFile = Path.GetTempFileName();
+                        var sr = new StreamReader(path);
+                        var sw = new StreamWriter(tempFile);
+                        for (int i = 0; i < lineIndex - 1; i++)
+                        {
+                            line = sr.ReadLine();
+                            if (line == null || line == "")
+                            {
+                                sw.WriteLine("");
+                            }
+                            else
+                            {
+                                sw.WriteLine(line);
+                            }
+                        }
+                        try
+                        {
+                            lineInput = string.Join(" ", lineInput.Split(' ').Skip(1));
+                            sw.WriteLine(lineInput);
+                            line = sr.ReadLine();
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                sw.WriteLine(line);
+                            }
+                            sr.Close();
+                            sw.Close();
+                            File.Delete(path);
+                            File.Move(tempFile, path);
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            line = sr.ReadLine();
+                            sw.WriteLine("");
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                sw.WriteLine(line);
+                            }
+                            sr.Close();
+                            sw.Close();
+                            File.Delete(path);
+                            File.Move(tempFile, path);
+                        }
+                    }
+                    else if (arr[0] == "+")
+                    {
+                        if (int.TryParse(arr[1], out lineIndex))
+                        {
+                            string tempFile = Path.GetTempFileName();
+                            var sr = new StreamReader(path);
+                            var sw = new StreamWriter(tempFile);
+                            for (int i = 0; i < lineIndex; i++)
+                            {
+                                line = sr.ReadLine();
+                                if (line == null || line == "")
+                                {
+                                    sw.WriteLine("");
+                                }
+                                else
+                                {
+                                    sw.WriteLine(line);
+                                }
+                            }
+                            try
+                            {
+                                lineInput = string.Join(" ", lineInput.Split(' ').Skip(2));
+                                sw.WriteLine(lineInput);
+                                line = sr.ReadLine();
+                                sw.WriteLine(line);
+                                while ((line = sr.ReadLine()) != null)
+                                {
+                                    sw.WriteLine(line);
+                                }
+                                sr.Close();
+                                sw.Close();
+                                File.Delete(path);
+                                File.Move(tempFile, path);
+                            }
+                            catch (IndexOutOfRangeException)
+                            {
+                                line = sr.ReadLine();
+                                sw.WriteLine("");
+                                sw.WriteLine(line);
+                                while ((line = sr.ReadLine()) != null)
+                                {
+                                    sw.WriteLine(line);
+                                }
+                                sr.Close();
+                                sw.Close();
+                                File.Delete(path);
+                                File.Move(tempFile, path);
+                            }
+                        }
+                    }
+                    else if (lineInput == ":clear")
+                    {
+                        Console.Clear();
+                    }
+                    else if (lineInput == ":clearfile")
+                    {
+                        File.WriteAllText(path, "");
+                    }
+                    else if (lineInput == ":removeemptylines")
+                    {
+                        string tempFile = Path.GetTempFileName();
+                        var sr = new StreamReader(path);
+                        var sw = new StreamWriter(tempFile);
+
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line != "")
+                            {
+                                sw.WriteLine(line);
+                            }
+                        }
+                        sr.Close();
+                        sw.Close();
+                        File.Delete(path);
+                        File.Move(tempFile, path);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid insert format.");
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # CMDEditor
+
+# Usage
+
+Enter file path (hit enter to use current directory)
+
+Enter the file name that you want to edit
+
+It should look like this:
+
+
+
+Hit enter
+
+To insert a line, specify the line where you want to insert a value. You can do this by specifying a positive integer, then writing the value of the line (For example, if you want to insert "Hello, World!" to line 10, use "10 Hello, World!")
+
+
+
+
+In case if you want to insert a line under a specified line, write "+", then the specified line as a positive integer and the line you want to insert. (For example, if you want to insert "Hello, World" under line 14, use "+ 14 Hello, World!")
+
+
+
+
+If you want to list the file that you're currently editing, hit enter without specifying anything.
+
+
+
+
+# Commands
+
+:q - Exits from the editor.
+
+:clear - Clears the console.
+
+:clearfile - Clears the file.
+
+:removeemptylines - Removes all the empty lines from the file.
